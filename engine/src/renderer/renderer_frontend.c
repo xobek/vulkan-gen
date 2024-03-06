23170cf6a2a2fa3cde0b14e27531c33d122bcc06
ashibot <50953882+xobek@users.noreply.github.com>
2024-03-06 21:34:07 +0100

    fix aspect ratio for render frame

--- engine/src/renderer/renderer_frontend.c
+++ engine/src/renderer/renderer_frontend.c
@@ -9,6 +9,9 @@ static renderer_backend* backend = 0;
 
 typedef struct renderer_system_state {
     renderer_backend backend;
+    mat4 projection;
+    f32 near_clip;
+    f32 far_clip;
 } renderer_system_state;
 
 static renderer_system_state* state_ptr;
@@ -29,6 +32,10 @@ b8 renderer_system_initialize(u64* memory_requirement, void* state, const char* 
         FATAL("Renderer failed to initialize!");
         return false;
     }
+    
+    state_ptr->near_clip = 0.1f;
+    state_ptr->far_clip = 1000.0f;
+    state_ptr->projection = mat4_perspective(deg_to_rad(45.0f), 1280 / 720.0f, state_ptr->near_clip, state_ptr->far_clip);
     return true;
 }
 
@@ -60,14 +67,13 @@ b8 renderer_end_frame(f32 delta_time) {
 b8 renderer_draw_frame(render_packet* packet) {
     if (renderer_begin_frame(packet->delta_time)) {
         
-        mat4 projection = mat4_perspective(deg_to_rad(45.0f), 1280 / 720.0f, 0.1f, 1000.0f);
 
         static f32 z = 0.0f;
         z += 0.01f;
         mat4 view = mat4_translation((vec3){0, 0, z});
         view = mat4_inverse(view);
 
-        state_ptr->backend.update_global_state(projection, view, vec3_zero(), vec4_one(), 0);
+        state_ptr->backend.update_global_state(state_ptr->projection, view, vec3_zero(), vec4_one(), 0);
 
         static f32 angle = 0.01f;
         angle += 0.001f;
@@ -87,6 +93,7 @@ b8 renderer_draw_frame(render_packet* packet) {
 
 void renderer_on_resized(u16 width, u16 height) {
     if (state_ptr) {
+        state_ptr->projection = mat4_perspective(deg_to_rad(45.0f), width / (f32)height, state_ptr->near_clip, state_ptr->far_clip);      
         state_ptr->backend.resized(&state_ptr->backend, width, height);
     }
 }
